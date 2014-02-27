a60c8133436951e73c1519f083058ba7b497a2ed
Sullivan990 <sullivan99o@yahoo.com>
2014-02-27 17:36:25 -0500

    Remove some placeholders

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source "http://www.rubygems.org"
 
 gem 'sinatra'
-gem 'smarter_csv'
 gem 'pry'
 gem 'shotgun'
--- server.rb
+++ server.rb
@@ -1,8 +1,22 @@
 require 'sinatra'
-require 'smarter_csv'
+require 'pry'
+require 'csv'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
-league = SmarterCSV.process('/Users/charliemsullivan/School/kickball_site/public/data.csv')
+set :views, File.dirname(__FILE__) + '/views'
+
+league = {}
+id = 0
+
+CSV.foreach("public/data.csv", headers: true) do |row|
+  league["player_#{id}".to_sym] = {
+    first_name: row[0],
+    last_name: row[1],
+    position: row[2],
+    team: row[3]
+  }
+  id += 1
+end
 
 get '/' do
   html = '''
@@ -12,22 +26,13 @@ get '/' do
       <title>LACKP League of Cool Kickball Professionals</title>
       <link rel="stylesheet" href="home.css" />
     </head>
+    <body>
+    <h1>League of Cool Kickball Professionals</h1>
+    <p>Placeholder</p>
+    </body>
   '''
 end
 
-get '/slammers' do
-  "<p>This is the fuckin roster</p>
-  <p>#{league}</p>"
-end
-
-get '/jets' do
-  "Jetson Jets"
-end
-
-get '/fire' do
-  "Flintstone Fire"
-end
-
-get '/goats' do
-  "Griphon Goats"
+get '/:team' do
+  erb :team
 end
